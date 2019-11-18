3ee8997f08b819a4c198b4f96261846020e1095b
= <=>
2019-11-18 11:53:11 +0530

    Fetch dates

--- components/Calender/Calender.js
+++ components/Calender/Calender.js
@@ -12,6 +12,7 @@ import {
 } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Button } from 'react-native-material-ui';
+import axios from 'axios';
 import { TextField } from 'react-native-materialui-textfield';
 
 class Calender extends React.Component {
@@ -22,19 +23,51 @@ class Calender extends React.Component {
             holiday: '',
             exam: ''
         }
-      }
+    }
+
+    fetchDates() {
+        // console.log('token', this.props.navigation.getParam('token', 'token'))
+        const headers = {
+            'Authorization': 'Bearer ' + this.props.navigation.getParam('token', 'token').access
+        }
+        axios({
+            method: 'GET',
+            url: 'https://201751025.pythonanywhere.com/dates/1',
+            headers: headers,
+        }).then((response) => {
+            console.log('resp', response.data)
+            this.setState({
+                academic: response.data.academic_link,
+                exam: response.data.exam_link,
+                holiday: response.data.holiday_link,
+            })
+
+        }).catch((error) => {
+            console.log(error)
+        });
+
+    }
+
+    componentDidMount() {
+        this.fetchDates()
+
+    }
+
+    componentDidUpdate() {
+        this.fetchDates()
+    }
 
     render() {
         return (
             <View KeyboardAvoidingView behaviour="padding" style={styles.container}>
                 <View style={styles.button}>
-                    <Button style={styles.button} onPress={ ()=> Linking.openURL(this.state.academic)} primary raised text="Academic Calender" />
+                    <Button style={styles.button} onPress={() => Linking.openURL(this.state.academic)} primary raised text="Academic Calender" />
                 </View>
                 <View style={styles.button}>
-                    <Button style={styles.button} onPress={ ()=> Linking.openURL(this.state.holiday)} primary raised text="Holiday Dates" />
+                    <Button style={styles.button} onPress={() => Linking.openURL(this.state.holiday)} primary raised text="Holiday Dates" />
                 </View>
                 <View style={styles.button}>
-                    <Button style={styles.button} onPress={ ()=> Linking.openURL(this.state.exam)} primary raised text="Exam Dates" />
+                    <Button style={styles.button} onPress={() => Linking.openURL(this.state.exam)} primary raised text="Exam Dates" />
                 </View>
             </View>
         );
